de8c81a04cf9710af51e2cd2eaeb7303a35c87b2
Jeff <Jeff01997@outlook.com>
2025-04-09 21:03:15 +0800

    Add try catch for debug auth

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,7 +1,14 @@
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  try {
+      const response = await authenticate.admin(request);
+      console.log('daniel  after authenticate.adminn');
+      const responseBody = await response.text(); // or response.json() if the response is JSON
+      console.log('Response body:', responseBody);
+  } catch (error) {
+      console.error('Error in  auth.$ loader', error);
+  }
 
   return null;
 };
